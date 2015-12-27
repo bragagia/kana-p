8144911e9ae7e624acb7d869c20f0b811f5f8811
Mathias Bragagia <mathias.bragagia@epitech.eu>
2015-12-27 22:37:16 +0100

    Corrects syntax

--- usr/share/kana-p/awesome/main.lua
+++ usr/share/kana-p/awesome/main.lua
@@ -253,7 +253,7 @@ mytaglist.buttons = awful.util.table.join(
 					awful.button({ }, 1, awful.tag.viewonly),
 					awful.button({ modkey }, 1, awful.client.movetotag),
 					awful.button({ }, 3, awful.tag.viewtoggle),
-					awful.button({ modkey }, 3, awful.client.toggletag)
+					awful.button({ modkey }, 3, awful.client.toggletag),
 					awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
 					awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
 					)
@@ -284,7 +284,7 @@ mytasklist.buttons = awful.util.table.join(
 													  theme = { width = 250 }
 												  })
 											  end
-										  end)
+										  end),
 					 awful.button({ }, 4, function ()
 											  awful.client.focus.byidx(1)
 											  if client.focus then client.focus:raise() end
