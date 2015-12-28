c316d183ce86a78c1edcccc8dc03bc3a10f49ece
Mathias Bragagia <mathias.bragagia@epitech.eu>
2015-12-28 21:59:29 +0100

    Removes unused code

--- usr/share/kana-p/awesome/main.lua
+++ usr/share/kana-p/awesome/main.lua
@@ -7,15 +7,6 @@ local wibox			= require("wibox")
 local beautiful		= require("beautiful")
 local lain			= require("lain")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-	naughty.notify({ preset = naughty.config.presets.critical,
-					 title = "Oops, there were errors during startup!",
-					 text = awesome.startup_errors })
-end
-
 -- Handle runtime errors after startup
 do
 	local in_error = false
