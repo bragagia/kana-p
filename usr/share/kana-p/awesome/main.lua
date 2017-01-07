a41ac8ccb29c85d539909059044005ee82c37324
Mathias Bragagia <mathias.bragagia@epitech.eu>
2017-01-08 00:23:43 +0100

    Removes buggy warning

--- usr/share/kana-p/awesome/main.lua
+++ usr/share/kana-p/awesome/main.lua
@@ -108,29 +108,29 @@ tags = {}
 doconf("tags.lua")
 
 -- battery warning
-local function trim(s)
-	return s:find'^%s*$' and '' or s:match'^%s*(.*%S)'
-end
-
-local function bat_notification()
-	local f_capacity = assert(io.open("/sys/class/power_supply/BAT0/capacity", "r"))
-	local f_status = assert(io.open("/sys/class/power_supply/BAT0/status", "r"))
-	local bat_capacity = tonumber(f_capacity:read("*all"))
-	local bat_status = trim(f_status:read("*all"))
-
-	if (bat_capacity <= 15 and bat_status == "Discharging") then
-		naughty.notify({ title      = "Battery Warning"
-		, text       = "Battery low ! " .. bat_capacity .."%" .. " left !"
-		, fg="#ffffff"
-		, bg="#C91C1C"
-		, timeout    = 15
-	})
-end
-end
-
-battimer = timer({timeout = 60})
-battimer:connect_signal("timeout", bat_notification)
-battimer:start()
+--local function trim(s)
+--	return s:find'^%s*$' and '' or s:match'^%s*(.*%S)'
+--end
+--
+--local function bat_notification()
+--	local f_capacity = assert(io.open("/sys/class/power_supply/BAT0/capacity", "r"))
+--	local f_status = assert(io.open("/sys/class/power_supply/BAT0/status", "r"))
+--	local bat_capacity = tonumber(f_capacity:read("*all"))
+--	local bat_status = trim(f_status:read("*all"))
+--
+--	if (bat_capacity <= 15 and bat_status == "Discharging") then
+--		naughty.notify({ title      = "Battery Warning"
+--		, text       = "Battery low ! " .. bat_capacity .."%" .. " left !"
+--		, fg="#ffffff"
+--		, bg="#C91C1C"
+--		, timeout    = 15
+--	})
+--end
+--end
+--
+--battimer = timer({timeout = 60})
+--battimer:connect_signal("timeout", bat_notification)
+--battimer:start()
 -- end here for battery warning
 
 local markup	  = lain.util.markup
