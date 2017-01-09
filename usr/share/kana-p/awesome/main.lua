9bde8975264b49cd2c4f519c6a472bfb07e70ce2
Mathias Bragagia <mathias.bragagia@epitech.eu>
2017-01-09 22:42:35 +0100

    Correct titlebar bug

--- usr/share/kana-p/awesome/main.lua
+++ usr/share/kana-p/awesome/main.lua
@@ -439,8 +439,106 @@ root.keys(globalkeys)
 
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
-awful.rules.rules = {}
-doconf("rules.lua")
+awful.rules.rules = {
+    -- All clients will match this rule.
+    {
+        rule = { },
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = awful.client.focus.filter,
+            raise = true,
+            keys = clientkeys,
+            buttons = clientbuttons,
+            screen = awful.screen.preferred,
+            placement = awful.placement.no_overlap+awful.placement.no_offscreen,
+        }
+    },
+
+    -- Floating clients.
+    {
+        rule_any = {
+            instance = {
+                "DTA",  -- Firefox addon DownThemAll.
+                "copyq",  -- Includes session name in class.
+            },
+            class = {
+                "Arandr",
+                "Gpick",
+                "Kruler",
+                "MessageWin",  -- kalarm.
+                "Sxiv",
+                "Wpa_gui",
+                "pinentry",
+                "veromix",
+                "xtightvncviewer",
+            },
+            name = {
+                "Event Tester",  -- xev.
+            },
+            role = {
+                "AlarmWindow",  -- Thunderbird's calendar.
+            }
+        },
+        properties = {
+            floating = true,
+        }
+    },
+
+    -- Add titlebars to normal clients and dialogs
+    {
+        rule_any = {
+            type = {
+                "normal",
+                "dialog",
+            }
+        },
+        properties = {
+            titlebars_enabled = true,
+        }
+    },
+
+    {
+        rule_any = {
+            class = {
+                "Vlc"
+            },
+        },
+        properties = {
+            sticky = true,
+            titlebars_enabled = false,
+        }
+    },
+    {
+        rule_any = {
+            class = {
+                "URxvt",
+                "XTerm",
+            }
+        },
+        properties = {
+            size_hints_honor = false,
+        }
+    },
+    {
+        rule = {
+            instance = "plugin-container"
+        },
+        properties = {
+            floating = true,
+            maximized_vertical = true,
+            maximized_horizontal = true,
+            focus = yes,
+            titlebars_enabled = false,
+        }
+    }
+
+    -- Set Firefox to always map on the tag named "2" on screen 1.
+    -- { rule = { class = "Firefox" },
+    --   properties = { screen = 1, tag = "2" } },
+}
+--awful.rules.rules = {}
+--doconf("rules.lua")
 -- }}}
 
 -- {{{ Signals
