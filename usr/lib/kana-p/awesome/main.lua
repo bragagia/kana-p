7aef2e99bc7a8fb5871417b9219b21d74ef3768a
Mathias Bragagia <mathias.bragagia@epitech.eu>
2017-01-13 22:43:44 +0100

    No rounded corner on maximized

--- usr/lib/kana-p/awesome/main.lua
+++ usr/lib/kana-p/awesome/main.lua
@@ -619,7 +619,11 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 --- {{{ Rounded window corner
 client.connect_signal("property::geometry", function (c)
   gears.timer.delayed_call(function()
-    gears.surface.apply_shape_bounding(c, gears.shape.rounded_rect, 5)
+		if c.maximized then
+				gears.surface.apply_shape_bounding(c, gears.shape.rectangle)
+		else
+				gears.surface.apply_shape_bounding(c, gears.shape.rounded_rect, 5)
+		end
   end)
 end)
 --- }}}
